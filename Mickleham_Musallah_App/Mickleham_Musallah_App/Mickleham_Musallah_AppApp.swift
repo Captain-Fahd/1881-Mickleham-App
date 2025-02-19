a871ea44f2c38a89f36cd484293863bc345688ea
Captain-Fahd <45980818+Captain-Fahd@users.noreply.github.com>
2025-02-19 14:08:59 +1100

    This commit provides the basic functions of the prayer app; the current time, and next prayer.

---
+++ Mickleham_Musallah_App/Mickleham_Musallah_App/Mickleham_Musallah_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mickleham_Musallah_AppApp.swift
+//  Mickleham_Musallah_App
+//
+//  Created by Omar Fahd on 19/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct Mickleham_Musallah_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mickleham_Musallah_App/Mickleham_Musallah_AppTests/Mickleham_Musallah_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mickleham_Musallah_AppTests.swift
+//  Mickleham_Musallah_AppTests
+//
+//  Created by Omar Fahd on 19/2/2025.
+//
+
+import Testing
+@testable import Mickleham_Musallah_App
+
+struct Mickleham_Musallah_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
